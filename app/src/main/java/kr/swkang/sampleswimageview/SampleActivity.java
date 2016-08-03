d87b4172eb1c4a980bc1b4b43266bee89423c228
ksu3101 <burkdog@naver.com>
2016-08-03 15:58:29 +0900

    [Added] more sample code. fix build bugs. and add bintray publish settings.

--- app/src/main/java/kr/swkang/sampleswimageview/SampleActivity.java
+++ app/src/main/java/kr/swkang/sampleswimageview/SampleActivity.java
@@ -1,7 +1,6 @@
 package kr.swkang.sampleswimageview;
 
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
