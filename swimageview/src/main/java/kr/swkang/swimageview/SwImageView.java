a645a0def190bc0b56ba2c54bd8d52b5f6e6a9ee
ksu3101 <burkdog@naver.com>
2016-08-03 10:52:27 +0900

    Initial commit

---
+++ swimageview/src/main/java/kr/swkang/swimageview/SwImageView.java
@@ -0,0 +1,28 @@
+package kr.swkang.swimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * @author KangSung-Woo
+ * @since 2016/08/03
+ */
+public class SwImageView
+    extends ImageView {
+
+  public SwImageView(Context context) {
+    super(context);
+  }
+
+  public SwImageView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public SwImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+
+
+}
